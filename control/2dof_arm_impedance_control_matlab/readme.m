05cec6ecde0e98880d4cd18759fb6f08a2c69c1d
Bharath Kumar <kumar7bharath@gmail.com>
2020-04-28 18:17:05 +0530

    impedence controller for 2dof arm in matlab

---
+++ control/2dof_arm_impedance_control_matlab/readme.m
@@ -0,0 +1,7 @@
+% Files:
+% main - Execute this file; parameters here.
+%
+% plotter -- Handles all integration and graphics 
+%
+% fullDyn -- Dynamics function of the form zdot = dynamics(z,t,params).
+% This function evaluates BOTH the controller and the dynamics.
